c9c180b6faab0a1db784feb1f5d7785b91701e6b
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-08-16 21:11:34 +0530

    Add JavaScript trivia bot lab

---
+++ Variables-and-Strings/02- Build a JavaScript Trivia Bot/Trivia-bot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Triviabot";
+let botLocation = "Trivia";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `some fact about ${favoriteLanguage}->`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is the most using language for developing.`;
+console.log(codingFact);
+
+codingFact = `some extra-ordinary functions are presents in the ${favoriteLanguage}.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
