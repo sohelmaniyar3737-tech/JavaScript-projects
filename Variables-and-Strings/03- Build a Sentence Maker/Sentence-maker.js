c01f12da2ced7fdac95c0ed231d31c7c8b9a37df
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-08-17 19:45:19 +0530

    Add sentence maker lab

---
+++ Variables-and-Strings/03- Build a Sentence Maker/Sentence-maker.js
@@ -0,0 +1,19 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "fly";
+let place = "castle";
+let adjective2 = "huge";
+let noun2 = "pizza";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+adjective = "funny";
+noun = "robot";
+verb = "dance";
+place = "laboratory";
+adjective2 = "tiny";
+noun2 = "noodles";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`\nSecond story: ${secondStory}`);
