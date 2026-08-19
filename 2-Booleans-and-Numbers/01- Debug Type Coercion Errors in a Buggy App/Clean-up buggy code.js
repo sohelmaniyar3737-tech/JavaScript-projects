426c850493d62a82fa7f5c926a997bc8b24b11f7
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-08-19 21:48:30 +0530

    Add code of cleaning the buggy code

---
+++ 2-Booleans-and-Numbers/01- Debug Type Coercion Errors in a Buggy App/Clean-up buggy code.js
@@ -0,0 +1,40 @@
+// Before correction
+
+const firstResult = "5" + 10;
+console.log(`5 + 10 = ${firstResult}`);
+
+const secondResult = 8 - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = true + 5;
+console.log(`true + 5 = ${thirdResult}`);
+
+const fourthResult = false + 8;
+console.log(`false + 8 = ${fourthResult}`);
+
+const fifthResult = "10" * 2;
+console.log(`"10" * 2 = ${fifthResult}`);
+
+const sixthResult = null + 22;
+console.log(`null + 22 = ${sixthResult}`);
+
+
+// After correction
+
+const firstResult = 5 + 10;
+console.log(`5 + 10 = ${firstResult}`);
+
+const secondResult = 8 - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 1 + 5;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 0 + 8;
+console.log(`0 + 8 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 0 + 22;
+console.log(`0 + 22 = ${sixthResult}`);
