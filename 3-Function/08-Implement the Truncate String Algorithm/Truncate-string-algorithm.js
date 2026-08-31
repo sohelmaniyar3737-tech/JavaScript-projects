72f9bd9100455813f0197428e9e6ea97c1b63275
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-08-31 19:32:06 +0530

    Add Truncate String Algorithm code

---
+++ 3-Function/08-Implement the Truncate String Algorithm/Truncate-string-algorithm.js
@@ -0,0 +1,11 @@
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  }
+
+  return str;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
