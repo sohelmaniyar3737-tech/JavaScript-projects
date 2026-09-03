90b1b81f6ee25225815cee7988c90f68e9e54dd7
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-09-03 21:21:01 +0530

    Add score translator code

---
+++ 4-Array/03-Build a Golf score translator/Score-translator.js
@@ -0,0 +1,23 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes === par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+}
+
+console.log(golfScore(1, 1));
+console.log(golfScore(5, 2));
+console.log(golfScore(3, 2));
