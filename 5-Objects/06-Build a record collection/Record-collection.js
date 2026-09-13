04702a202845a40fe2d2cc19c21ea755aa2e598a
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-09-13 18:15:07 +0530

    Add record collection code

---
+++ 5-Objects/06-Build a record collection/Record-collection.js
@@ -0,0 +1,39 @@
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } 
+  else if (prop === "tracks") {
+    if (!records[id].hasOwnProperty("tracks")) {
+      records[id].tracks = [];
+    }
+
+    records[id].tracks.push(value);
+  } 
+  else {
+    records[id][prop] = value;
+  }
+
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
