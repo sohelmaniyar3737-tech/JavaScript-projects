f7f757f46bb762cd07afd76215d518be983d80e2
Sohel Maniyar <sohelmaniyar3737@gmail.com>
2026-09-23 19:48:49 +0530

    Add the string repeating function code

---
+++ 6-Loops/11-Build a String Repeating Function/String-repeating-function.js
@@ -0,0 +1,16 @@
+function repeatStringNumTimes(str, num) {
+  let result = "";
+
+  if (num <= 0) {
+    return result;
+  }
+
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+
+  return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3))
+console.log(repeatStringNumTimes("abc", 5))
